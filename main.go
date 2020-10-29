388d11f8707f1913ba7dbe23203a7c6e608e1299
idsulik@gmail.com <idsulik@gmail.com>
2020-10-29 11:48:44 +0300

    Init

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"gotemplate/server"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	port := 8080
+
+	if portStr, found := os.LookupEnv("PORT"); found {
+		var err error
+		port, err = strconv.Atoi(portStr)
+
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	log.Fatal(server.New(port))
+}
---
+++ server/server.go
@@ -0,0 +1,74 @@
+package server
+
+import (
+	"bytes"
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"html/template"
+	"net/http"
+)
+
+type data struct {
+	Variables string
+	Template string
+	Result string
+	Error error
+}
+
+type Values struct {
+	items interface{}
+}
+
+var t *template.Template
+
+func New(port int) error {
+	var err error
+	t, err = template.ParseFiles("server/template.html")
+
+	if err != nil {
+		panic(err)
+	}
+
+	http.HandleFunc("/", mainHandler)
+	http.HandleFunc("/render", renderHandler)
+
+	return http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
+
+func mainHandler(writer http.ResponseWriter, _ *http.Request) {
+	if err := t.Execute(writer, nil); err != nil {
+		panic(err)
+	}
+}
+
+func renderHandler(writer http.ResponseWriter, request *http.Request) {
+	userVariablesStr := request.PostFormValue("variables")
+	userTemplateStr := request.PostFormValue("template")
+	var userVariables interface{}
+
+	if err := yaml.Unmarshal([]byte(userVariablesStr), &userVariables); err != nil {
+		panic(err)
+	}
+
+	userTemplate, err := template.New("userTemplate").Parse(userTemplateStr)
+
+	if err != nil {
+		panic(err)
+	}
+
+	data := data{
+		Variables: userVariablesStr,
+		Template:  userTemplateStr,
+	}
+
+	var tpl bytes.Buffer
+	if err := userTemplate.Execute(&tpl, userVariables); err != nil {
+		data.Error = err
+	}
+
+	data.Result = tpl.String()
+
+	if err := t.Execute(writer, data); err != nil {
+		panic(err)
+	}
+}
